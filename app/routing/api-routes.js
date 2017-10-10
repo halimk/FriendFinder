046d218482a0329e6a83b62ad86f435477553c61
halimk <kalhlmk@aol.com>
2017-10-10 11:18:00 -0400

    friendfinder

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,10 +1,6 @@
 var friendList = require('../data/friends.js');
 var path = require('path');
 
-//==============================================
-//ROUTING
-//==============================================
-
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
 		res.json(friendList);
--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type:'application/vnd.api+json'}));
 
 
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
 	console.log('App listening on PORT ' + process.env.PORT);
 });
